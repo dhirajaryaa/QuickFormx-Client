d33aed2dd55d4204b4c7c99ef1000b50441a3ed4
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-04-13 12:07:20 +0000

    feat: adding dynamic link active tab

--- src/components/custom/Navbar.jsx
+++ src/components/custom/Navbar.jsx
@@ -4,30 +4,35 @@ import {
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
-} from "@/components/ui/sidebar"
-import { useSelector } from "react-redux"
-import {Link} from "react-router-dom"
+} from "@/components/ui/sidebar";
+import { NavLink } from "react-router-dom";
 
 function Navbar({ navMainMenu }) {
-  const {activeTab} = useSelector(state=>state.ui);  
+  
   return (
     <SidebarGroup>
       <SidebarGroupContent className="flex flex-col gap-2">
         <SidebarMenu>
           {navMainMenu.map((item) => (
-            <Link key={item.title} to={item.url}>
-            <SidebarMenuItem  className={'mt-1'}>
-              <SidebarMenuButton tooltip={item.title} className={`py-6 text-sm sm:text-[16px] font-base  hover:bg-blue-100/50 ${activeTab === item.title ? "bg-blue-200/50":""}`}>
-                {item.icon}
-                <span>{item.title}</span>
-              </SidebarMenuButton>
+            <SidebarMenuItem className="mt-1" key={item.title}>
+              <NavLink to={item.url}>
+                {({ isActive }) => (
+                  <SidebarMenuButton
+                    tooltip={item.title}
+                    className={`py-6 text-sm sm:text-[16px] font-base hover:bg-blue-100/50 ${isActive ? "bg-blue-200/50" : ""
+                      }`}
+                  >
+                    {item.icon}
+                    <span>{item.title}</span>
+                  </SidebarMenuButton>
+                )}
+              </NavLink>
             </SidebarMenuItem>
-            </Link>
           ))}
         </SidebarMenu>
       </SidebarGroupContent>
     </SidebarGroup>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
