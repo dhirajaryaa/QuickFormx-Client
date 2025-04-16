2a389ac42a8da6c669c19c566467a28a301bddbb
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-04-16 14:51:07 +0000

    refactor : fix placeholder hidden on  not used input

--- src/components/custom/forms/DynamicField.jsx
+++ src/components/custom/forms/DynamicField.jsx
@@ -6,22 +6,22 @@ import { FieldSelector } from '..';
 import { useDispatch } from 'react-redux';
 import { deleteField, updateFieldLabel, updateFieldPlaceholder, updateFieldRequired } from '@/app/features/formBuilderSlice';
 
-function DynamicField({ field: { id, label,type, placeholder, required } }) {
+function DynamicField({ field: { id, label, type, placeholder, required } }) {
     const dispatch = useDispatch();
 
     const handleDelete = (id) => {
         dispatch(deleteField(id))
     };
 
-    const handleLabelChange = (id,value) => {
-        dispatch(updateFieldLabel({id,label:value}));
+    const handleLabelChange = (id, value) => {
+        dispatch(updateFieldLabel({ id, label: value }));
     };
 
-    const handlePlaceholderChange = (id,value) => {
-        dispatch(updateFieldPlaceholder({id,placeholder:value}));
+    const handlePlaceholderChange = (id, value) => {
+        dispatch(updateFieldPlaceholder({ id, placeholder: value }));
     };
 
-    const handleRequiredToggle = (id)=>{
+    const handleRequiredToggle = (id) => {
         dispatch(updateFieldRequired(id))
     }
 
@@ -32,22 +32,28 @@ function DynamicField({ field: { id, label,type, placeholder, required } }) {
                 <div className='flex items-start flex-col'>
                     <Label htmlFor={`label_${id}`} className={'text-sm sm:text-base '}>Field Name {required && <span className='text-destructive'>*</span>} </Label>
                     <input
-                    value={label}
-                    onChange={(e)=>handleLabelChange(id,e.target.value)}
-                    id={`label_${id}`} className={"border-0 w-full outline-0 border-b-2"} />
+                        value={label}
+                        onChange={(e) => handleLabelChange(id, e.target.value)}
+                        id={`label_${id}`} className={"border-0 w-full outline-0 border-b-2"} />
                 </div>
                 {/* field selector  */}
                 <FieldSelector id={id} type={type} />
             </div>
             {/* placeholder  */}
-            <div className='w-full my-4'>
-                <Label htmlFor={`placeholder_${id}`}>Placeholder </Label>
-                <input 
-                id={`placeholder_${id}`}
-                value={placeholder}
-                onChange={(e)=>handlePlaceholderChange(id,e.target.value)}
-                className={"mt-1 text-sm border-0 w-full outline-0 border-b-2"} placeholder='Enter hint text' />
-            </div>
+            {["text", "textarea", "number", "email"].includes(type) && (
+                <div className='w-full my-4'>
+                    <Label htmlFor={`placeholder_${id}`}>Placeholder</Label>
+                    <input
+                        id={`placeholder_${id}`}
+                        value={placeholder}
+                        onChange={(e) => handlePlaceholderChange(id, e.target.value)}
+                        className="mt-1 text-sm border-0 w-full outline-0 border-b-2"
+                        placeholder="Enter hint text"
+                        required
+                    />
+                </div>
+            )}
+
 
             <div className="flex items-center justify-between gap-2 mt-3">
                 {/* delete btn  */}
